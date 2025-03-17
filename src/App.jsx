edcccdc3e6678df139ae46e9a2b98a3f63cae4cf
Abhijit Kumar <abhijitkumar10.5@gmail.com>
2025-03-17 22:31:27 +0530

    config: tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Hello Badge Component</h1>
+  return (
+    <h1 className='text-3xl text-amber-300 text-center'>
+      Hello Badge Component
+    </h1>
+  )
 }
 
 export default App
